0b27e3454ba2678b7231033404ebe259320f69a1
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2024-11-13 10:25:16 -0300

    iniciado o seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslist.entities;
 
+
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -19,9 +20,13 @@ public class Game {
 	private String title;
 	@Column(name = "game_year")
 	private Integer year;
+	private String genre;
 	private String platforms;
+	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -30,13 +35,15 @@ public class Game {
 	}
 
 
-	public Game(Long id, String title, Integer year, String platforms, String imgUrl, String shortDescription,
-			String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
+		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -73,6 +80,16 @@ public class Game {
 	}
 
 
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
 	public String getPlatforms() {
 		return platforms;
 	}
@@ -80,6 +97,16 @@ public class Game {
 
 	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
@@ -130,6 +157,7 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 	
