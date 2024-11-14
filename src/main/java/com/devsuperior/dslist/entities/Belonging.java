ace344ca6babd84e84302402afb5f70744d0fcf4
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2024-11-13 22:22:26 -0300

    domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,62 @@
+package com.devsuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+	
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	private Integer posistion;
+	
+	public Belonging () {
+	}
+
+	public Belonging(Game game,  GameList list, Integer posistion) {
+		
+		id.setGame(game);
+		id.setList(list);
+		this.posistion = posistion;
+	}
+
+	public BelongingPK getId() {
+		return id;
+	}
+
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+
+	public Integer getPosistion() {
+		return posistion;
+	}
+
+	public void setPosistion(Integer posistion) {
+		this.posistion = posistion;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
