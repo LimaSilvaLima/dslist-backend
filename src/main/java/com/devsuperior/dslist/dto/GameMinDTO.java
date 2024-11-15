2ae30160dda7f0b32db5b31487ed760a69ba9365
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2024-11-14 22:59:50 -0300

    get list/id/games

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,10 +1,9 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
-
-
 	
 	private Long id;
 	private String title;
@@ -24,6 +23,17 @@ public class GameMinDTO {
 		this.imgUrl = entity.getImgUrl();
 		this.shortDescription = entity.getShortDescription();
 	}
+
+
+	public GameMinDTO(GameMinProjection projection) {
+		
+		this.id = projection.getId();
+		this.title = projection.getTitle();
+		this.year = projection.getYear();
+		this.imgUrl = projection.getImgUrl();
+		this.shortDescription = projection.getShortDescription();
+	}
+
 
 	public Long getId() {
 		return id;
